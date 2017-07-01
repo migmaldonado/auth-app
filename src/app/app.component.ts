ac01e8697a0456cce5c8682b46c79c90f7af96e7
Miguel <maldesan@gmail.com>
2017-07-01 17:05:19 -0400

    eod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { SessionService } from "./session.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  formInfo = {
+    username: '',
+    password: ''
+  };
+
+  user: any;
+  error: string;
+  privateData: any;
+
+  constructor(private session: SessionService) { }
+
+  login() {
+    this.session.login(this.formInfo)
+      .subscribe(
+        (user) => this.user = user,
+        (err) => this.error = err
+      );
+  }
+
+  signup() {
+    this.session.signup(this.formInfo)
+      .subscribe(
+        (user) => this.user = user,
+        (err) => this.error = err
+      );
+  }
+
+  logout() {
+    this.session.logout()
+      .subscribe(
+        () => this.user = null,
+        (err) => this.error = err
+      );
+  }
+
+  getPrivateData() {
+    this.session.getPrivateData()
+      .subscribe(
+        (data) => this.privateData = data,
+        (err) => this.error = err
+      );
+  }
+
+  errorCb(err) {
+    this.error = err;
+    this.user = null;
+  }
+
+successCb(user) {
+    this.user = user;
+    this.error = null;
+  }
+
 }
